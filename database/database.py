334d783f114165f6c4fc88b4db8703b640fbdbf5
Rahul N <rahul.nagarajmv@gmail.com>
2022-01-01 21:34:25 -0600

    change db url

--- database/database.py
+++ database/database.py
@@ -5,8 +5,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = os.environ.get('DATABASE_URL')
-if (DATABASE_URL.startswith("postgres")):
-    DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql", 1)
+# if (DATABASE_URL.startswith("postgres")):
+#     DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql", 1)
 engine = create_engine(DATABASE_URL, echo=False)
 
 # Create database if it does not exist.
